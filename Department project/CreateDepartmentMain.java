2575d241fdab127416236fd7b7dd5320551b4b3c
rasi-priya <58032882+rasi-priya@users.noreply.github.com>
2019-12-03 18:21:15 +0530

    Add files via upload

---
+++ Department project/CreateDepartmentMain.java
@@ -0,0 +1,30 @@
+
+package com.hcl.DepartmentProject;
+import java.util.List;
+
+
+
+public class CreateDepartmentMain {
+	
+	public static void main(String[] args) {
+		DepartmentDAO dao=new DepartmentDAO();
+		List<Department>departmentList=dao.showDepartment();
+		for (Department department : departmentList) {
+			System.out.println("Deptno="+department.getDeptno());
+			System.out.println("Dname="+department.getDname());
+			System.out.println("Loc= "+department.getLoc());
+			System.out.println("City= "+department.getCity());
+			System.out.println("Head= "+department.getHead());
+			System.out.println("--------------------------");
+			
+		}
+		
+	}
+
+}
+
+
+
+
+
+
---
+++ Department project/DeleteDepartmentMain.java
@@ -0,0 +1,23 @@
+package com.hcl.DepartmentProject;
+
+import java.util.Scanner;
+
+
+
+public class DeleteDepartmentMain {
+	public static void main(String[] args) {
+		int deptno;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter employ no..");
+	    deptno=sc.nextInt();
+	    DepartmentDAO dao=new DepartmentDAO();
+		System.out.println(dao.deleteDepartment(deptno));
+	
+		
+		
+		
+	}
+
+}
+
+
